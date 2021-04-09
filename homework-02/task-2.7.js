74a6d4b672ce5bfd5050cccfc5ee61fac09a438d
Pavel Omelchenko <p.omel4enko2002@gmail.com>
2021-04-09 06:18:07 +0300

    Изменил корень проекта

--- homework-02/task-2.7.js
+++ homework-02/task-2.7.js
@@ -1,14 +1,9 @@
-const cards = [
-  'Карточка-1',
-  'Карточка-2',
-  'Карточка-3',
-  'Карточка-4',
-  'Карточка-5',
-];
+const slugify = string => {
+  return string.split(' ').join('-').toLowerCase();
+};
 
-const cardToRemove = 'Карточка-3';
-const index = cards.indexOf(cardToRemove);
-cards.push('last element');
-cards.unshift('first element');
-cards.splice(1, 1, 'replace element');
-console.table(cards);
+console.log(slugify('Top 10 dfg dfg dfgdfGFD dGGsdf osdf sdf'));
+console.log(slugify('Top 10 dfg dhfghHFGg dfgdfGFD dGGsdf osdf sdfdfgdfg'));
+console.log(
+  slugify('Top 10 dfg dfg dfgdfGFD dGGsdf oHf sdfg Hfgd fHzGFFf F FHGHg'),
+);
